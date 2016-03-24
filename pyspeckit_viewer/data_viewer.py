61a2e762108c7a2c829995d446abbac9a0d3c243
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-03-24 11:13:05 -0700

    Added Matplotlib canvas, figure, and axes

--- pyspeckit_viewer/data_viewer.py
+++ pyspeckit_viewer/data_viewer.py
@@ -1,4 +1,5 @@
 from glue.viewers.common.qt.data_viewer import DataViewer
+from glue.viewers.common.qt.mpl_widget import MplWidget
 
 class PyspeckitViewer(DataViewer):
 
@@ -6,7 +7,9 @@ class PyspeckitViewer(DataViewer):
 
     def __init__(self, session, parent=None):
         super(PyspeckitViewer, self).__init__(session, parent=parent)
-        # self.setCentralWidget(my_qt_widget)
+        self._mpl_widget = MplWidget()
+        self._mpl_axes = self._mpl_widget.canvas.fig.add_subplot(1,1,1)
+        self.setCentralWidget(self._mpl_widget)
 
     def add_data(self, data):
         return True
