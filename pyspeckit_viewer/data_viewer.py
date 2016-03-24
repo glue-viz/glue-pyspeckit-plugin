936ca909f23ca4bd66ba5ff19e629ba61d46e72c
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-03-24 16:20:27 -0700

    Added radio buttons for modes

--- pyspeckit_viewer/data_viewer.py
+++ pyspeckit_viewer/data_viewer.py
@@ -14,7 +14,7 @@ from glue.viewers.common.qt.mpl_widget import MplWidget
 from glue.viewers.common.qt.toolbar import GlueToolbar
 from glue.core.roi import RectangularROI, XRangeROI
 from glue.viewers.common.qt.mouse_mode import (RectangleMode, HRangeMode,)
-from glue.utils.qt.widget_properties import CurrentTabProperty
+from glue.utils.qt.widget_properties import CurrentTabProperty, ButtonProperty
 
 from .viewer_options import OptionsWidget
 
@@ -25,6 +25,12 @@ class PyspeckitViewer(DataViewer):
 
     mode = CurrentTabProperty('_control_panel.tab_mode')
 
+    line_identify = ButtonProperty('_control_panel.radio_line_peak')
+    line_select = ButtonProperty('_control_panel.radio_line_selection')
+
+    cont_select = ButtonProperty('_control_panel.radio_cont_selection')
+    cont_exclude = ButtonProperty('_control_panel.radio_cont_exclusion')
+
     def __init__(self, session, parent=None):
         super(PyspeckitViewer, self).__init__(session, parent=parent)
 
@@ -33,6 +39,18 @@ class PyspeckitViewer(DataViewer):
 
         self._control_panel = load_ui('control_panel.ui', None,
                                 directory=os.path.dirname(__file__))
+
+        self._line_mode = QtGui.QButtonGroup()
+        self._line_mode.addButton(self._control_panel.radio_line_peak)
+        self._line_mode.addButton(self._control_panel.radio_line_selection)
+
+        self._line_mode.buttonClicked.connect(nonpartial(self._update_line_mode))
+
+        self._cont_mode = QtGui.QButtonGroup()
+        self._cont_mode.addButton(self._control_panel.radio_cont_selection)
+        self._cont_mode.addButton(self._control_panel.radio_cont_exclusion)
+
+        self._cont_mode.buttonClicked.connect(nonpartial(self._update_cont_mode))
 
         self._options_widget = OptionsWidget(data_viewer=self)
 
@@ -46,6 +64,18 @@ class PyspeckitViewer(DataViewer):
         self.toolbar = self.make_toolbar()
 
         self._control_panel.tab_mode.currentChanged.connect(nonpartial(self.set_mode))
+
+    def _update_line_mode(self):
+        if self.line_identify:
+            print("Identify mode")
+        elif self.line_select:
+            print("Select mode")
+
+    def _update_cont_mode(self):
+        if self.cont_select:
+            print("Select mode")
+        elif self.cont_exclude:
+            print("Exclude mode")
 
     def set_mode(self):
         if self.mode == 'Fit Line':
